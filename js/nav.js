1d11c8c817628227c2fcf27381d5bb9b575824e0
gyuhyeongH <ckacl792@gmail.com>
2022-07-21 21:27:42 +0900

    fix : 네비바 필요없는 부분 삭제

--- js/nav.js
+++ js/nav.js
@@ -13,37 +13,3 @@ $(document).ready(function () {
     }
 });
 
-window.onload = () => {
-};
-
-$(function () {
-    $('#include_nav').load("nav.html", function () {
-    });
-
-    $('#include_login').load("login.html", function () {
-        model_html = makeModal('Login', '400', '550', $('#include_login').html());
-        $('#include_login').empty()
-        $('#include_login').append(model_html)
-    });
-
-    $('#include_signup').load("signup.html", function () {
-        model_html = makeModal('Signup', '400', '635', $('#include_signup').html());
-        $('#include_signup').empty()
-        $('#include_signup').append(model_html)
-    });
-
-    modal_html = makeModal('WhyClick', '600', '700', $('#whyclick').html());
-    $('#whyclick').empty()
-    $('#whyclick').append(modal_html)
-});
-
-function showSignup() {
-    $('#loginModal').modal('hide');
-    $('#signupModal').modal('show');
-}
-
-function showLogin() {
-    $('#signupModal').modal('hide');
-    $('#loginModal').modal('show');
-}
-
