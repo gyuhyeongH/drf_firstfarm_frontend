20db9fdf1cba65d8e9f3f5c7039a0cfde49164eb
gyuhyeongH <ckacl792@gmail.com>
2022-07-21 21:54:17 +0900

    feat : get하기전 하위요소들 삭제, get 게시물 작성 1차완료

--- js/main.js
+++ js/main.js
@@ -18,21 +18,39 @@ let menu_list = ['', '', '', '', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1
 
 function get_article(choice) {
     category = document.getElementsByClassName('nav-link active')[0].value
-    console.log(category)
-    console.log(choice)
     $.ajax({
         headers: { "choice": choice, "category": category },
         type: "GET",
         url: "http://127.0.0.1:8000/article/",
         data: {},
         success: function (response) {
-            console.log(response)
+            $('#get_article').empty();
             for (let i = 0; i < response.length; i++) {
+                console.log(i)
                 let title = response[i]['title']
-                let img_url = response[i]['img_url']
-                let user = response[i]['user']
-                let article_id = response[i]['id']
-                let temp_g1;
+                let location = response[i]['location']
+                let cost = response[i]['cost']
+                let exposure_end_date = response[i]['exposure_end_date'].substr(0, 10)
+                let updated_at = response[i]['updated_at'].substr(0, 10)
+                let temp_article = `<div class="articles">
+                <div class="contents">
+                    ${location}
+                </div>
+                <div class="contents">
+                ${title}
+                </div>
+                <div class="contents">
+                ${cost}
+                </div>
+                <div class="contents">
+                ${exposure_end_date}
+                </div>
+                <div class="contents">
+                ${updated_at}
+                </div>
+            </div>`
+                $('#get_article').append(temp_article);
+
             }
         }
     })
