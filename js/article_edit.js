66f66da8477059155cec481db509bc8b324cf67b
HWISU96 <soshyzx123@naver.com>
2022-08-01 14:45:19 +0900

    feat : 게시글 수정 기능 추가(미완성)

---
+++ js/article_edit.js
@@ -0,0 +1,166 @@
+function put_articledetail(article_id, i) {
+  let title = $("#edit_title" + i).val();
+  let farm_name = $("#edit_farm_name" + i).val();
+  let location = $("#edit_location" + i).val();
+  let cost = $("#edit_cost" + i).val();
+  let period = $("#edit_period" + i).val();
+  let requirement = $("#edit_requirement" + i).val();
+  let desc = $("#edit_desc" + i).val();
+  let img1 = $("#edit_img1" + i).val();
+  let img2 = $("#edit_img2" + i).val();
+  let img3 = $("#edit_img3" + i).val();
+  let article_category = $("#edit_" + i).val();
+
+  $.ajax({
+    type: "PUT",
+    url: "http://127.0.0.1:8000/article/detail/" + article_id,
+    data: {
+      title: title,
+      farm_name: farm_name,
+      location: location,
+      cost: cost,
+      period: period,
+      requirement: requirement,
+      desc: desc,
+      img1: img1,
+      img2: img2,
+      img3: img3,
+      article_category: article_category,
+    },
+
+    success: function (response) {
+      let temp_detail_put;
+
+      temp_detail_put = `
+            <button type="button" class="submit_button" onclick="put_articledetail()">수정하기</button>
+            <button type="button" class="cancel_button">취소</button>
+            `;
+      $("#submit_button").append(temp_detail_put);
+
+      alert("업데이트 완료");
+      window.location.reload();
+    },
+  });
+}
+
+function sample4_execDaumPostcode() {
+  new daum.Postcode({
+    oncomplete: function (data) {
+      // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+      // 도로명 주소의 노출 규칙에 따라 주소를 표시한다.
+      // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+      var roadAddr = data.roadAddress; // 도로명 주소 변수
+      var extraRoadAddr = ""; // 참고 항목 변수
+
+      // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+      // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+      if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
+        extraRoadAddr += data.bname;
+      }
+      // 건물명이 있고, 공동주택일 경우 추가한다.
+      if (data.buildingName !== "" && data.apartment === "Y") {
+        extraRoadAddr +=
+          extraRoadAddr !== "" ? ", " + data.buildingName : data.buildingName;
+      }
+      // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+      if (extraRoadAddr !== "") {
+        extraRoadAddr = " (" + extraRoadAddr + ")";
+      }
+
+      // 도로명 주소 정보를 해당 필드에 넣는다.
+      document.getElementById("sample4_roadAddress").value = roadAddr;
+    },
+  }).open();
+}
+
+// 카테고리 선택.
+$(document).ready(function () {
+  $("#category1").click(function () {
+    $("input#article_category").val("1");
+  });
+});
+
+$(document).ready(function () {
+  $("#category2").click(function () {
+    $("input#article_category").val("2");
+  });
+});
+
+// 이미지 선택하는 부분.
+const reader = new FileReader();
+reader.onload = (readerEvent) => {
+  document
+    .querySelector("#img_section")
+    .setAttribute("src", readerEvent.target.result);
+  //파일을 읽는 이벤트가 발생하면 img_section의 src 속성을 readerEvent의 결과물로 대체함
+};
+
+document.querySelector("#img").addEventListener("change", (changeEvent) => {
+  //upload_file 에 이벤트리스너를 장착
+
+  const imgFile = changeEvent.target.files[0];
+  reader.readAsDataURL(imgFile);
+  //업로드한 이미지의 URL을 reader에 등록
+});
+
+const reader2 = new FileReader();
+reader2.onload = (readerEvent) => {
+  document
+    .querySelector("#img_section2")
+    .setAttribute("src", readerEvent.target.result);
+  //파일을 읽는 이벤트가 발생하면 img_section의 src 속성을 readerEvent의 결과물로 대체함
+};
+
+document.querySelector("#img2").addEventListener("change", (changeEvent) => {
+  //upload_file 에 이벤트리스너를 장착
+
+  const imgFile = changeEvent.target.files[0];
+  reader2.readAsDataURL(imgFile);
+  //업로드한 이미지의 URL을 reader에 등록
+});
+
+const reader3 = new FileReader();
+reader3.onload = (readerEvent) => {
+  document
+    .querySelector("#img_section3")
+    .setAttribute("src", readerEvent.target.result);
+  //파일을 읽는 이벤트가 발생하면 img_section의 src 속성을 readerEvent의 결과물로 대체함
+};
+
+document.querySelector("#img3").addEventListener("change", (changeEvent) => {
+  //upload_file 에 이벤트리스너를 장착
+
+  const imgFile = changeEvent.target.files[0];
+  reader3.readAsDataURL(imgFile);
+  //업로드한 이미지의 URL을 reader에 등록
+});
+
+// 버튼 클릭 확인을 위한 색상 추가하는 부분.
+var category_button = document.getElementsByClassName("category_button");
+
+function handleClick(event) {
+  // console.log(event.target);
+  // console.log(this);
+  // 콘솔창을 보면 둘다 동일한 값이 나온다
+
+  // console.log(event.target.classList);
+
+  if (event.target.classList[1] === "clicked") {
+    event.target.classList.remove("clicked");
+  } else {
+    for (var i = 0; i < category_button.length; i++) {
+      category_button[i].classList.remove("clicked");
+    }
+
+    event.target.classList.add("clicked");
+  }
+}
+
+function init() {
+  for (var i = 0; i < category_button.length; i++) {
+    category_button[i].addEventListener("click", handleClick);
+  }
+}
+
+init();
