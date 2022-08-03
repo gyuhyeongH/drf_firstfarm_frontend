ead7bd981d03f19f9d0b2098740c1e76bfaa6d19
imjuyoung <im_joo0@naver.com>
2022-08-04 00:14:14 +0900

    feat : backend url 변경

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,5 @@
-const backend_base_url = "http://127.0.0.1:8000";
-// const backend_base_url = "http://3.35.37.28:8000";
+// const backend_base_url = "http://127.0.0.1:8000";
+const backend_base_url = "http://3.35.37.28:8000";
 const frontend_base_url = "http://127.0.0.1:5500";
 
 
