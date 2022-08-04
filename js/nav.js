82ae4e1d58f28a66450e64be30fdb323d7087958
gyuhyeongH <ckacl792@gmail.com>
2022-08-04 12:09:21 +0900

    fix : 오류나는 부분 삭제, 재귀되는 코드 삭제

--- js/nav.js
+++ js/nav.js
@@ -12,17 +12,10 @@ $(document).ready(function () {
         $('#a_mypage').show();
     }
 
-    if (payload.category == 1 ) {
-        $('#a_article').hide();
-    }
-    else {
-        $('#a_article').show();
-    }
-    
+
 });
 
 async function handle_enter_mypage() {
-<<<<<<< HEAD
 
     const payload = JSON.parse(localStorage.getItem("payload"));
     const user_category = payload.category;
@@ -32,16 +25,5 @@ async function handle_enter_mypage() {
         window.location.replace(`https://polite-paprenjak-e2afb5.netlify.app/farm.html`);
     } else {
         window.location.replace(`https://polite-paprenjak-e2afb5.netlify.app/farmer.html`);
-=======
-    console.log("handle_enter_mypage()");
-
-    const payload = JSON.parse(localStorage.getItem("payload"));
-    const user_category = payload.category;
-
-    if (user_category == 1) {
-        window.location.replace(`http://127.0.0.1:5500/farm.html`);
-    } else {
-        window.location.replace(`http://127.0.0.1:5500/farmer.html`);
->>>>>>> a2c1a64cc451c20be8ffc37278784ae68e7faaa1
     }
 }
