b20a3342d31bed0c664a393b0695f5756a37455a
DHL68 <ldh1255@naver.com>
2022-08-07 17:04:16 +0900

    feat : nav.js 코드 수정 - refresh token 갱신 코드 개선 : try 구문 사용으로 첫 번째 오류 발생 방지 - auth.js 의 로그아웃 기능을 nav.js 로 이동, html 의 불필요한 script 삭제

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,6 @@
-const payload = JSON.parse(localStorage.getItem("payload"));
 
 $(document).ready(function () {
+    const payload = JSON.parse(localStorage.getItem("payload"));
     if (payload == null) {
         $('#a_logout').hide();
         $('#a_mypage').hide();
@@ -24,4 +24,62 @@ async function handle_enter_mypage() {
             window.location.replace(`https://polite-paprenjak-e2afb5.netlify.app/farmer.html`);
         }
     }
+}
+
+window.onload = () => {
+    const payload = JSON.parse(localStorage.getItem("payload"));
+
+    // 여기에 try 를 넣으면 어떠한가?
+    try {
+        // 아직 access 토큰의 인가 유효시간이 남은 경우
+        if (payload.exp > (Date.now() / 1000)) {
+        } else {
+            // 인증 시간이 지났기 때문에 다시 refreshToken으로 다시 요청을 해야 한다.
+            const requestRefreshToken = async (url) => {
+                console.log(url)
+                console.log(payload.refresh)
+                const response = await fetch(url, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    method: "POST",
+                    body: JSON.stringify({
+                        "refresh": localStorage.getItem("refresh_token")
+                    })
+                }
+                );
+                return response.json();
+            };
+
+
+            // 다시 인증 받은 accessToken을 localStorage에 저장하자.
+            requestRefreshToken(`${backend_base_url}/user/api/token/refresh/`).then((data) => {
+                // 새롭게 발급 받은 accessToken을 localStorage에 저장
+                const accessToken = data.access;
+                const refreshToken = data.refresh;
+                localStorage.setItem("access_token", accessToken);
+
+                const base64Url = data.access.split('.')[1];
+                const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+                const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+                    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+                }).join(''));
+                localStorage.setItem("payload", jsonPayload);
+                localStorage.setItem("refresh_token", refreshToken);
+                console.log("성공!! : " + accessToken);
+            });
+            }
+    }
+    catch {
+    }
+};
+
+// 로그아웃
+async function handle_logout() {
+    localStorage.removeItem("access");
+    localStorage.removeItem("refresh");
+    localStorage.removeItem("payload");
+    alert("로그아웃 되었습니다.");
+    window.location.replace(`${frontend_base_url}/signin.html`);
+    // location.reload()
 }
