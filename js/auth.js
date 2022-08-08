c8ff12f4a5f1b51626eaffe3e2a196c930ba8eb4
DHL68 <ldh1255@naver.com>
2022-08-08 13:50:10 +0900

    feat : 주소 변경 및 주석 정리

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,5 @@
-// const backend_base_url = "https://rbgud.shop";
-// const frontend_base_url = "https://polite-paprenjak-e2afb5.netlify.app";
-const backend_base_url = "http://127.0.0.1:8000";
-const frontend_base_url = "http://127.0.0.1:5500";
+const backend_base_url = "https://rbgud.shop";
+const frontend_base_url = "https://polite-paprenjak-e2afb5.netlify.app";
 
 // 카테고리 토글
 var category_btn = document.getElementsByClassName("category_btn");
--- js/nav.js
+++ js/nav.js
@@ -29,49 +29,45 @@ async function handle_enter_mypage() {
 window.onload = () => {
     // 여기에 try 를 넣으면 어떠한가?
     const payload = JSON.parse(localStorage.getItem("payload"));
-    try {
-        console.log("갱신 시작");
-        // 아직 access 토큰의 인가 유효시간이 남은 경우
-        if (payload.exp > (Date.now() / 1000)) {
-        } else {
-            // 인증 시간이 지났기 때문에 다시 refreshToken으로 다시 요청을 해야 한다.
-            const requestRefreshToken = async (url) => {
-                console.log(url)
-                const response = await fetch(url, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    method: "POST",
-                    body: JSON.stringify({
-                        "refresh": localStorage.getItem("refresh_token")
-                    })
-                }
-                );
-                return response.json();
-            };
+    console.log("갱신 시작");
+    // 아직 access 토큰의 인가 유효시간이 남은 경우
+    if (payload != null) {
+        payload.exp > (Date.now() / 1000)
+    } else {
+        // 인증 시간이 지났기 때문에 다시 refreshToken으로 다시 요청을 해야 한다.
+        const requestRefreshToken = async (url) => {
+            console.log(url)
+            const response = await fetch(url, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    "refresh": localStorage.getItem("refresh_token")
+                })
+            }
+            );
+            return response.json();
+        };
 
-            // 다시 인증 받은 accessToken을 localStorage에 저장하자.
-            requestRefreshToken(`http://127.0.0.1:8000/user/api/token/refresh/`).then((data) => {
-                // 새롭게 발급 받은 accessToken을 localStorage에 저장
-                const accessToken = data.access;
-                const refreshToken = data.refresh;
-                localStorage.setItem("access_token", accessToken);
+        // 다시 인증 받은 accessToken을 localStorage에 저장하자.
+        requestRefreshToken(`https://rbgud.shop/user/api/token/refresh/`).then((data) => {
+            // 새롭게 발급 받은 accessToken을 localStorage에 저장
+            const accessToken = data.access;
+            const refreshToken = data.refresh;
+            localStorage.setItem("access_token", accessToken);
 
-                const base64Url = data.access.split('.')[1];
-                const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-                const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-                    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-                }).join(''));
-                localStorage.setItem("payload", jsonPayload);
-                localStorage.setItem("refresh_token", refreshToken);
-                console.log("성공!! : " + accessToken);
-            });
-        }
+            const base64Url = data.access.split('.')[1];
+            const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+            const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+            }).join(''));
+            localStorage.setItem("payload", jsonPayload);
+            localStorage.setItem("refresh_token", refreshToken);
+            console.log("성공!! : " + accessToken);
+        });
     }
-    catch {
-        console.log("갱신 실패")
-    }
-};
+}
 
 // 로그아웃
 async function handle_logout() {
@@ -79,6 +75,5 @@ async function handle_logout() {
     localStorage.removeItem("refresh");
     localStorage.removeItem("payload");
     alert("로그아웃 되었습니다.");
-    window.location.replace(`http://127.0.0.1:5500/index2.html`);
-    // location.reload()
+    window.location.replace(`https://polite-paprenjak-e2afb5.netlify.app/index2.html`);
 }
