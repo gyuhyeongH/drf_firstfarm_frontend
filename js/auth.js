2df76cdc988bae4f3e327acb1e083440160878bf
imjuyoung <im_joo0@naver.com>
2022-08-09 13:03:23 +0900

    feat : 오류 수정

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,7 @@
 const backend_base_url = "https://rbgud.shop";
 const frontend_base_url = "https://polite-paprenjak-e2afb5.netlify.app";
-
+// const backend_base_2_url = "http://127.0.0.1:8000";
+// const frontend_base_2_url = "http://127.0.0.1:5500";
 // 카테고리 토글
 var category_btn = document.getElementsByClassName("category_btn");
 var user_category_value = document.getElementById("category_value")
